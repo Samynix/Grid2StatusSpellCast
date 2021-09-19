1af4ad1982c05228a54efef33d1300da1775d6f6
Tor Einar Gudmestad <samynix@gmail.com>
2021-09-19 21:31:20 +0200

    cleanup

--- Grid2StatusSpellCast.lua
+++ Grid2StatusSpellCast.lua
@@ -1,14 +1,16 @@
 local CastTypeSpell = 'spell'
 local CastTypeChannel = 'channel'
 
+local TIMEOUT = 40 --Max time an indicator can be shown is 40 sec.
+
 local StatusSpellCast = Grid2.statusPrototype:new("spell-cast", false) 
 StatusSpellCast._eventFrame = CreateFrame("frame")
 StatusSpellCast._isEnabled = false
 StatusSpellCast._defaultDB = {
     interval = 0.1,
     special_spells = {
-        [30128] = { extra_duration = 2 },
-        [32938] = { extra_duration = 2 }
+        -- [30128] = { extra_duration = 2 },
+        -- [32938] = { extra_duration = 2 }
     },
     mode = "all",
     ignoredSpellList = {},
@@ -39,34 +41,6 @@ Grid2:DbSetStatusDefaultValue("spell-cast", {type = "spell-cast", color1 = {r=0,
 local prev_LoadOptions = Grid2.LoadOptions
 function Grid2:LoadOptions()
     Grid2Options:RegisterStatusOptions("spell-cast", "combat", function(self, status, options)
-        -- self.RDO:Init()
-        -- local empty = not (next(GSRD.db.profile.enabledModules) or next(GSRD.db.profile.debuffs))
-        -- local function GetSpellID(name, defaultSpells) --Taken from StatusHealsAoe
-        --     if tonumber(name) then
-        --         return tonumber(name)
-        --     end
-        --     for _,spells in next, defaultSpells do
-        --         for _,spell in next, spells do
-        --             local spellName = GetSpellInfo(spell)
-        --             if spellName == name then
-        --                 return spell
-        --             end
-        --         end
-        --     end
-        --     local id = 0
-        --     local texture = select(3, GetSpellInfo(name))
-        --     for i=300000, 1, -1  do
-        --         if GetSpellInfo(i) == name then
-        --             id = i
-        --             local _,_,tex = GetSpellInfo(i)
-        --             if tex == texture then
-        --                 return i
-        --             end
-        --         end
-        --     end
-        --     return id
-        -- end
-
         options.general = {
                 type = "group",
                 name = "General Settings",
@@ -228,14 +202,12 @@ function StatusSpellCast:OnUpdate()
         nameplate = "nameplate" .. tostring(i)
         if UnitExists(nameplate) then
             StatusSpellCast:HandleEnemeyUnit(nameplate)
-            --StatusSpellCast:HandleEnemeyUnit(nameplate, UnitChannelInfo)
         end
     end
 
     for i, unitId in pairs(StatusSpellCast.unit_ids) do
         if UnitExists(unitId) then
             StatusSpellCast:HandleEnemeyUnit(nameplate)
-            --StatusSpellCast:HandleEnemeyUnit(nameplate)
         end
     end
 end
@@ -268,9 +240,6 @@ function StatusSpellCast:HandleEnemeyUnit(unitId)
         return
     end
 
-    -- if self._active_indicators[unitInformation.destinationUnitId] then
-    --     return
-    -- end
     if not UnitIsEnemy("player", unitId) then
         return false
     end
@@ -293,15 +262,14 @@ function StatusSpellCast:HandleEnemeyUnit(unitId)
         end
     end
     
-    -- spell, text, icon, startTime, endTime, _, spellIdChannel, spellIdCast = castInfoFunction(unitInformation.sourceUnitId)
     if not spell or not IsSpellRelevant(spell) then
         return false
     end
 
     special_spell = StatusSpellCastDB.special_spells[spellId]
     extra_duration = special_spell and special_spell.extra_duration or 0
-    -- print(spellId, extra_duration, special_spell)
-    self._active_indicators[unitInformation.destinationUnitId] = {
+
+    self._active_indicators[unitInformation.destinationGuid] = {
         icon = icon,
         start = startTime/1000,
         duration = (endTime-startTime) / 1000 + (StatusSpellCastDB.interval/2) + extra_duration,
@@ -309,79 +277,41 @@ function StatusSpellCast:HandleEnemeyUnit(unitId)
         text = text,
         unitInformation = unitInformation,
         endTime = endTime/1000 + (StatusSpellCastDB.interval/2) + extra_duration,
-        castType = castType
+        castType = castType,
+        timeout = GetTime() + TIMEOUT
     }
 
     self._npc_guids_outgoing_spells[unitInformation.sourceGuid] = unitInformation
     self._player_guids_incoming_spells[unitInformation.destinationGuid] = unitInformation
 
-    self:UpdateIndicators(unitInformation.destinationUnitId)
+    self:UpdateIndicators(unitInformation.getDestinationUnitId())
     return true
 end
 
 function StatusSpellCast:DeactiveIndicatorsByTime(currentTime)
-    for unitId, indicatorData in pairs(self._active_indicators) do
-        if (indicatorData.endTime <= currentTime) then
-            self._active_indicators[unitId] = nil
-
+    for unitGuid, indicatorData in pairs(self._active_indicators) do
+        if (indicatorData.endTime <= currentTime or indicatorData.timeout <= currentTime) then
             self:ResetVariablesForIndicator(indicatorData.unitInformation)
-            self:UpdateIndicators(unitId)
         end
     end
 end
-
-
-
-
--- function StatusSpellCast:HandleCastStart(castInfoFunction, unitId, castGuid, spellId)
---     if string.find(unitId, "boss") then
---         print('BossCasting', unitId)
---     end
-
---     unitInformation = self:GetSpellEventUnitInformation(unitId)
---     if not unitInformation then
---         return
---     end
-
---     if self._active_indicators[unitInformation.destinationUnitId] then
---         return
---     end
-
---     if not self:IsHostileNpcUnit(unitInformation.sourceGuid) then
---         return
---     end
-
---     spell, text, icon, startTime, endTime = castInfoFunction(unitInformation.sourceUnitId)
---     self._active_indicators[unitInformation.destinationUnitId] = {
---         icon = icon,
---         start = startTime/1000,
---         duration = (endTime-startTime) / 1000 + (self.db.profile.interval/2),
---         ICON_TEX_COORDS,
---         text = text,
---         unitInformation = unitInformation,
---         endTime = endTime/1000 + (self.db.profile.interval/2)
---     }
-
---     self._player_incoming_spells[unitInformation.sourceGuid] = unitInformation
---     self._player_guids_incoming_spells[unitInformation.destinationGuid] = unitInformation
-
---     self:UpdateIndicators(unitInformation.destinationUnitId)
--- end
 
 function StatusSpellCast:GetSpellEventUnitInformation(sourceUnitId)
     local sourceTargetUnitId = sourceUnitId .. "target"
     
     local sourceGuid = UnitGUID(sourceUnitId)
     local destinationGuid = UnitGUID(sourceTargetUnitId)
-    local destinationUnitId = Grid2:IsGUIDInRaid(destinationGuid)
-    if (not destinationUnitId) then
+    if (not Grid2:IsGUIDInRaid(destinationGuid)) then
         return nil
     end
 
     return {
         sourceGuid = sourceGuid,
-        sourceUnitId = sourceUnitId,
-        destinationUnitId = destinationUnitId,
+        sourceUnitId = sourceUnitId, --Should not be used at a later timer, only on this tick, may change
+        getDestinationUnitId = function() 
+            local id = Grid2:IsGUIDInRaid(destinationGuid)
+            return id
+        end,
         destinationGuid = destinationGuid,
     }
 end
@@ -398,20 +328,17 @@ function StatusSpellCast:HandleSpellCastAborted(unitId, castGuid, spellId)
     end
 
     self:ResetVariablesForIndicator(unitInformation, true)
-    self:UpdateIndicators(unitInformation.destinationUnitId)
 end
 
 function StatusSpellCast:IsHostileNpcUnit(guid)
     return not Grid2:IsGUIDInRaid(guid)
 end
 
-function StatusSpellCast:ResetVariablesForIndicator(unitInformation, isExtendChannel)
-    -- if isExtendChannel then
-    --     indicator = self._active_indicators[unitInformation.destinationUnitId]
-    -- end
-    self._active_indicators[unitInformation.destinationUnitId] = nil
+function StatusSpellCast:ResetVariablesForIndicator(unitInformation)
+    self._active_indicators[unitInformation.destinationGuid] = nil
     self._npc_guids_outgoing_spells[unitInformation.sourceGuid] = nil
     self._player_guids_incoming_spells[unitInformation.destinationGuid] = nil
+    self:UpdateIndicators(unitInformation.getDestinationUnitId())
 end
 
 function StatusSpellCast:OnDisable()
@@ -432,21 +359,46 @@ end
 
 --Grid status indicator getters
 function StatusSpellCast:IsActive(unitId)
-	return self._active_indicators[unitId] ~= nil
+    local guid = UnitGUID(unitId)
+    if (not guid) then
+        return nil
+    end
+
+	return self._active_indicators[guid] ~= nil
 end
 
 function StatusSpellCast:GetDuration(unitId)
-	return self._active_indicators[unitId].duration
+    local guid = UnitGUID(unitId)
+    if (not guid) then
+        return nil
+    end
+
+	return self._active_indicators[guid].duration
 end
 
 function StatusSpellCast:GetExpirationTime(unitId)
-	return self._active_indicators[unitId].endTime
+    local guid = UnitGUID(unitId)
+    if (not guid) then
+        return nil
+    end
+
+	return self._active_indicators[guid].endTime
 end
 
 function StatusSpellCast:GetIcon(unitId)
-	return self._active_indicators[unitId].icon
+    local guid = UnitGUID(unitId)
+    if (not guid) then
+        return nil
+    end
+
+	return self._active_indicators[guid].icon
 end
 
 function StatusSpellCast:GetText(unitId)
-	return self._active_indicators[unitId].text
+    local guid = UnitGUID(unitId)
+    if (not guid) then
+        return nil
+    end
+
+	return self._active_indicators[guid].text
 end
